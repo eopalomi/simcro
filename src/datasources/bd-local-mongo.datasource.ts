cd2b6d42f13a281ae7d716b8c1526231ce11a250
Erick <eopalomi@gmail.com>
2022-11-09 18:10:17 -0500

    add connection mongo servidores

--- src/datasources/bd-local-mongo.datasource.ts
+++ src/datasources/bd-local-mongo.datasource.ts
@@ -1,17 +1,31 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
+// const config = {
+//   name: 'bd_local_mongo',
+//   connector: 'mongodb',
+//   url: '',
+//   host: 'localhost',
+//   port: 27017,
+//   user: '',
+//   password: '',
+//   database: 'cronograma_db',
+//   useNewUrlParser: true
+// };
+
 const config = {
   name: 'bd_local_mongo',
   connector: 'mongodb',
-  url: '',
-  host: 'localhost',
-  port: 27017,
-  user: '',
-  password: '',
-  database: 'cronograma_db',
+  url: 'mongodb://admin:mongoFortt22@10.3.3.122:27020/cronograma_db?authSource=admin',
+  // host: '10.3.3.122',
+  // port: 27020,
+  // user: 'admin',
+  // password: 'mongoFortt22',
+  // database: 'cronograma_db',
+  tls: true,
   useNewUrlParser: true
 };
+
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
